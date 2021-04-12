abcee7131ab61c1667c42412b51f1b3c9b78f104
Alexandr <gatt2508@yandex.ru>
2021-04-12 19:22:54 +0300

    Small refactoring of Tests

--- posts/tests.py
+++ posts/tests.py
@@ -10,13 +10,60 @@ class TestClient:
         self.client = Client()
         self.username = 'testuser'
         self.password = 'difficult_password'
-        self.user = User.objects.create(username=self.username)
-        self.user.set_password(self.password)
-        self.user.save()
+        self.user = User.objects.create_user(username=self.username, password=self.password)
         self.client.login(username=self.username, password=self.password)
+        self.title = 'Тестовый заголовок'
+        self.text = 'Текст тестового поста'
+        self.slug = 'test_slug'
+        self.urls_to_check = {'post_view': [self.username, self.slug], 'profile': [self.username], 'main_page': []}
 
     def tearDown(self):
         self.user.delete()
+
+
+class PostTests(TestClient, TestCase):
+    def test_publication_on_all_pages(self):
+        title = 'Тестовый заголовок'
+        text = 'Текст тестового поста'
+        slug = 'test_slug'
+        self.client.login(username=self.username, password=self.password)
+        self.client.post(reverse('create_post'), data={
+            'title': title,
+            'text': text,
+            'slug': slug
+        })
+
+        for url in self.urls_to_check:
+            response = self.client.get(reverse(url, args=self.urls_to_check[url]))
+            self.assertContains(response, title and text)
+
+
+class UserPublishPost(TestClient, TestCase):
+    def test_auth_user_can_post(self):
+        response = self.client.get(reverse('create_post'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_not_auth_user_cant_post(self):
+        self.client.get(reverse('logout'))
+        response = self.client.get(reverse('create_post'))
+        self.assertEqual(response.status_code, 302)
+
+    def test_auth_user_can_edit_and_all_pages_change(self):
+        title_new = 'Тестовый новый заголовок'
+        text_new = 'Новый текст тестового поста'
+        Post.objects.create(author=self.user,
+                            title=self.title,
+                            text=self.text,
+                            slug=self.slug)
+        self.client.get(reverse('post_edit', args=[self.username, self.slug]))
+        self.client.post(reverse('post_edit', args=[self.username, self.slug]), data={
+            'title': title_new,
+            'text': text_new
+        })
+        urls_to_check = {'post_view': [self.username, self.slug], 'profile': [self.username], 'main_page': []}
+        for url in urls_to_check:
+            response = self.client.get(reverse(url, args=urls_to_check[url]))
+            self.assertContains(response, title_new and text_new)
 
 
 class RegistrationAndProfilePageTests(TestCase):
@@ -37,55 +84,3 @@ class RegistrationAndProfilePageTests(TestCase):
         response_profile = self.client.get('/{0}/'.format(self.username))
         self.assertEqual(response_profile.status_code, 200)
         self.assertContains(response_profile, self.username)
-
-
-class PostTests(TestClient, TestCase):
-    def test_publication_on_all_pages(self):
-        title = 'Тестовый заголовок'
-        text = 'Текст тестового поста'
-        slug = 'test_slug'
-        self.client.login(username=self.username, password=self.password)
-        self.client.post(reverse('create_post'), data={
-            'title': title,
-            'text': text,
-            'slug': slug
-        })
-        response_post_view = self.client.get(reverse('post_view', args=[self.username, slug]))
-        self.assertContains(response_post_view, title and text)
-        response_profile = self.client.get(reverse('profile', args=[self.username]))
-        self.assertContains(response_profile, title and text)
-        response_main_page = self.client.get(reverse('main_page'))
-        self.assertContains(response_main_page, title and text)
-
-
-class UserPublishPost(TestClient, TestCase):
-    def test_auth_user_can_post(self):
-        response = self.client.get(reverse('create_post'))
-        self.assertEqual(response.status_code, 200)
-
-    def test_not_auth_user_cant_post(self):
-        self.client.get(reverse('logout'))
-        response = self.client.get(reverse('create_post'))
-        self.assertEqual(response.status_code, 302)
-
-    def test_auth_user_can_edit_and_all_pages_change(self):
-        title = 'Тестовый заголовок'
-        text = 'Текст тестового поста'
-        slug = 'test_slug'
-        title_new = 'Тестовый новый заголовок'
-        text_new = 'Новый текст тестового поста'
-        Post.objects.create(author=self.user,
-                            title=title,
-                            text=text,
-                            slug=slug)
-        self.client.get(reverse('post_edit', args=[self.username, slug]))
-        response_edit = self.client.post(reverse('post_edit', args=[self.username, slug]), data={
-            'title': title_new,
-            'text': text_new
-        })
-        response_post_view = self.client.get(reverse('post_view', args=[self.username, slug]))
-        self.assertContains(response_post_view, title_new and text_new)
-        response_profile = self.client.get(reverse('profile', args=[self.username]))
-        self.assertContains(response_profile, title_new and text_new)
-        response_main_page = self.client.get(reverse('main_page'))
-        self.assertContains(response_main_page, title_new and text_new)
